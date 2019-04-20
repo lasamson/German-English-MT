41760bcc1737777ddb99d2f46984003fce19d080
Aarsh Patel <aarshpatel@Aarshs-MacBook-Pro.local>
2019-04-20 11:49:23 -0400

    Added a max length

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -26,14 +26,17 @@ def load_dataset(data_path, min_freq=5, train_batch_size=32, dev_batch_size=1):
     SRC = Field(tokenize=tokenize_de, init_token=None, eos_token="</s>", batch_first=True, include_lengths=True)
     TRG = Field(tokenize=tokenize_en, init_token="<s>", eos_token="</s>", batch_first=True, include_lengths=True)
 
+    MAX_LEN = 50
     train_data, dev_data = datasets.TranslationDataset.splits(exts=(".de", ".en"),
-                                    fields=(SRC, TRG), path=data_path, test=None, validation="dev")
+                                    fields=(SRC, TRG), path=data_path, test=None, validation="dev", 
+                                    filter_pred=lambda x: len(vars(x)['src']) <= MAX_LEN and 
+                                    len(vars(x)['trg']) <= MAX_LEN))
 
     SRC.build_vocab(train_data.src, min_freq=min_freq)
     TRG.build_vocab(train_data.trg, min_freq=min_freq)
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     train_iterator = BucketIterator(train_data, batch_size=train_batch_size, train=True, 
                                     sort_within_batch=True, sort_key=lambda x: (len(x.src), len(x.trg)),
                                     repeat=False, device=device)
