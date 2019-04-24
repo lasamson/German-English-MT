f5ab7deca741d4a65cb801603553da23311f77fa
Aarsh Patel <aarshpatel@Aarshs-MacBook-Pro.local>
2019-04-24 19:51:07 -0400

    make config

--- make_config.py
+++ make_config.py
@@ -27,16 +27,15 @@ if __name__ == '__main__':
     p.add_argument('-dvbsz', '--dev_batch_size', type=int, default=1, help='Dev/Test batch size')
     p.add_argument('-embsz', '--embed_size', type=int, help='Embedding dimension', required=True)
     p.add_argument('-hidsz', '--hidden_size', type=int, help='Hidden size', required=True)
-    p.add_argument('-nenc', '--n_layers_enc', type=int, default=1, help='Num layers in encoder')
-    p.add_argument('-ndec', '--n_layers_dec', type=int, default=1, help='Num layers in decoder')
+    p.add_argument('-nenc', '--n_layers_enc', type=int, default=2, help='Num layers in encoder')
+    p.add_argument('-ndec', '--n_layers_dec', type=int, default=2, help='Num layers in decoder')
     p.add_argument('-maxlen', '--max_length', type=int, default=50, help='Max length of sequence')
     p.add_argument('-lr', '--lr', type=float, default=.001, help='Learning rate')
     p.add_argument('-gc', '--grad_clip', type=float, default=5.0, help='Gradient clipping threshold')
-    p.add_argument('-tf', '--teacher_forcing_ratio', type=float, default=0.5, help='Teacher forcing ratio') 
-    p.add_argument('-attention', '--attention', type=str, default="None", help='Attention applied to Seq2Seq')
+    p.add_argument('-tf', '--teacher_forcing_ratio', type=float, default=1.0, help='Teacher forcing ratio') 
+    p.add_argument('-attention', '--attention', type=str, default=None, help='Attention applied to Seq2Seq')
     p.add_argument('-svoc_size', '--src_vocab_size', type=int, default=30000, help='Source vocab size')
     p.add_argument('-tvoc_size', '--tgt_vocab_size', type=int, default=30000, help='Target vocab size')
-    p.add_argument('-dmod', '--d_model', type=int, default=512, help='Transformer hidden size')
     p.add_argument('-nheads', '--num_heads', type=int, default=8, help='Number of transformer attention heads')
     p.add_argument('-inpdrop', '--input_dropout', type=float, default=0.1, help='Input dropout')
     p.add_argument('-laydrop', '--layer_dropout', type=float, default=0.1, help='Layer dropout')
@@ -45,6 +44,7 @@ if __name__ == '__main__':
     p.add_argument('-decweightshare', '--tgt_emb_prj_weight_sharing', type=bool, default=False, help='Whether to weight tie decoder embed/proj weights')
     p.add_argument('-encdecweightshare', '--emb_src_tgt_weight_sharing', type=bool, default=False, help='Whether to weight tie encoder and decoder weights')
     p.add_argument('-labsmooth', '--label_smoothing', type=float, default=0.1, help='Label smoothing rate')
+    p.add_argument('-dff', '--d_ff', type=float, default=0.1, help='Size of intermiediate hidden layer in Positionwise Feed Forward Net')
     p.add_argument('-warmup', '--n_warmup_steps', type=int, default=4000, help='Number of warmup steps for learning rate')
     p.add_argument('-exp_name', '--experiment_name', type=str, help='Experiment name', required=True)
     p.add_argument('-model_type', '--model_type', type=str, help='Type of model', required=True)
@@ -58,9 +58,8 @@ if __name__ == '__main__':
                     'layer_dropout', 'tgt_emb_prj_weight_sharing', 'emb_src_tgt_weight_sharing', 'exp_name', 'model_type']
 
     model2args = {
-        's2s': common_params + ['hidden_size'],
-        's2s_attn': common_params + ['hidden_size', 'attention'],
-        'transformer': common_params + ['src_vocab_size', 'tgt_vocab_size', 'd_model', 'num_heads',
+        'GRU': common_params + ['hidden_size', 'attention'],
+        'Transformer': common_params + ['src_vocab_size', 'tgt_vocab_size', 'd_model', 'num_heads',
                                         'attention_dropout', 'relu_dropout', 'label_smoothing',
                                         'n_warmup_steps']
     }
@@ -72,6 +71,3 @@ if __name__ == '__main__':
         print('Directory', dir_name, 'already exists. Rewriting configuration parameters.')
     finally:
         write_params(args, dir_name, model2args)
-
-
-
