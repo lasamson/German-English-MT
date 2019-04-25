1477a8a820e6e5af79f78401f87f07fa5c15e4cc
Aarsh Patel <aarshpatel@Aarshs-MacBook-Pro.local>
2019-04-25 00:09:41 -0400

    models/

--- make_config.py
+++ make_config.py
@@ -23,9 +23,9 @@ if __name__ == '__main__':
     p = argparse.ArgumentParser(description='Set up directory for experiments')
     p.add_argument('-e', '--epochs', type=int, help='Number of epochs to train', required=True)
     p.add_argument('-mf', '--min_freq', type=int, default=5, help='Minimum frequency cutoff for word to be included in vocab')
-    p.add_argument('-trbsz', '--train_batch_size', type=int, default=32, help='Training batch size')
+    p.add_argument('-trbsz', '--train_batch_size', type=int, default=64, help='Training batch size')
     p.add_argument('-dvbsz', '--dev_batch_size', type=int, default=1, help='Dev/Test batch size')
-    p.add_argument('-embsz', '--embed_size', type=int, help='Embedding dimension', required=True)
+    p.add_argument('-embsz', '--embedding_size', type=int, help='Embedding dimension', required=True)
     p.add_argument('-hidsz', '--hidden_size', type=int, help='Hidden size', required=True)
     p.add_argument('-nenc', '--n_layers_enc', type=int, default=2, help='Num layers in encoder')
     p.add_argument('-ndec', '--n_layers_dec', type=int, default=2, help='Num layers in decoder')
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     p.add_argument('-decweightshare', '--tgt_emb_prj_weight_sharing', type=bool, default=False, help='Whether to weight tie decoder embed/proj weights')
     p.add_argument('-encdecweightshare', '--emb_src_tgt_weight_sharing', type=bool, default=False, help='Whether to weight tie encoder and decoder weights')
     p.add_argument('-labsmooth', '--label_smoothing', type=float, default=0.1, help='Label smoothing rate')
-    p.add_argument('-dff', '--d_ff', type=float, default=0.1, help='Size of intermiediate hidden layer in Positionwise Feed Forward Net')
+    p.add_argument('-dff', '--d_ff', type=int, default=2048, help='Size of intermiediate hidden layer in Positionwise Feed Forward Net')
     p.add_argument('-warmup', '--n_warmup_steps', type=int, default=4000, help='Number of warmup steps for learning rate')
     p.add_argument('-exp_name', '--experiment_name', type=str, help='Experiment name', required=True)
     p.add_argument('-model_type', '--model_type', type=str, help='Type of model', required=True)
@@ -53,13 +53,13 @@ if __name__ == '__main__':
 
     dir_name = './experiments/' + args.experiment_name
 
-    common_params = ['epochs', 'min_freq', 'train_batch_size', 'dev_batch_size', 'embed_size', 'n_layers_enc', 
+    common_params = ['epochs', 'min_freq', 'train_batch_size', 'dev_batch_size', 'embedding_size', 'n_layers_enc', 
                     'n_layers_dec', 'max_length', 'lr', 'grad_clip', 'teacher_forcing_ratio', 'input_dropout',
                     'layer_dropout', 'tgt_emb_prj_weight_sharing', 'emb_src_tgt_weight_sharing', 'exp_name', 'model_type']
 
     model2args = {
         'GRU': common_params + ['hidden_size', 'attention'],
-        'Transformer': common_params + ['src_vocab_size', 'tgt_vocab_size', 'd_model', 'num_heads',
+        'Transformer': common_params + ['src_vocab_size', 'tgt_vocab_size', 'hidden_size', 'num_heads',
                                         'attention_dropout', 'relu_dropout', 'label_smoothing',
                                         'n_warmup_steps']
     }
